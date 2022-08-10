5685a20deec82a8dff11535387302d6c41188ead
asep <asep@kct.co.id>
2022-08-10 18:10:51 +0700

    push golang mini project

---
+++ assets/scripts.js
@@ -0,0 +1,40 @@
+<script type="text/javascript">
+    $(document).ready(function () {
+        // alert("tess");
+        // $('#confirm-delete').on('show.bs.modal', function(e) {
+        //     $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+            
+        //     $('.debug-url').html('Delete URL: <strong>' + $(this).find('.btn-ok').attr('href') + '</strong>');
+        // });
+        // $('#PersonTableContainer').jtable({
+        //     title: 'Table of people',
+        //     actions: {
+        //         listAction: '/GettingStarted/PersonList',
+        //         createAction: '/GettingStarted/CreatePerson',
+        //         updateAction: '/GettingStarted/UpdatePerson',
+        //         deleteAction: '/GettingStarted/DeletePerson'
+        //     },
+        //     fields: {
+        //         PersonId: {
+        //             key: true,
+        //             list: false
+        //         },
+        //         Name: {
+        //             title: 'Author Name',
+        //             width: '40%'
+        //         },
+        //         Age: {
+        //             title: 'Age',
+        //             width: '20%'
+        //         },
+        //         RecordDate: {
+        //             title: 'Record date',
+        //             width: '30%',
+        //             type: 'date',
+        //             create: false,
+        //             edit: false
+        //         }
+        //     }
+        // });
+    });
+</script>
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "html/template"
+    "path"
+    "strings"
+	"strconv"
+)
+
+type Response struct {
+    Success string
+    Message string
+}
+
+type Task struct {
+    Id          string
+    TaskName    string
+    Assignee  string
+    Date string
+}
+
+var tasks []Task;
+
+func main() {
+	fmt.Println("Test golang progate")
+	fmt.Println("Hello World")
+	http.HandleFunc("/", handlerIndex)
+    http.HandleFunc("/create", createTask)
+    http.HandleFunc("/save", saveTask)
+    http.HandleFunc("/update/", updateTask)
+    http.HandleFunc("/delete/", deleteTask)
+
+	http.Handle("/static/",
+        http.StripPrefix("/static/",
+            http.FileServer(http.Dir("assets"))))
+
+    var address = "localhost:9090"
+    fmt.Printf("server started at %s\n", address)
+    err := http.ListenAndServe(address, nil)
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+}
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	var filepath = path.Join("views", "index.html")
+	var tmpl = template.Must(template.ParseFiles(filepath))
+	if err := tmpl.Execute(w, map[string]interface{}{"tasks":tasks}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+    var filepath = path.Join("views", "form.html")
+	var tmpl = template.Must(template.New("form").ParseFiles(filepath))
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+    id := strings.TrimPrefix(r.URL.Path, "/update/")
+    var selectedTask Task
+    for i := range tasks {
+        if tasks[i].Id == id {
+            selectedTask = tasks[i]
+        }
+    }
+
+    var filepath = path.Join("views", "form.html")
+	var tmpl = template.Must(template.New("form").ParseFiles(filepath))
+	if err := tmpl.Execute(w, selectedTask); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func saveTask(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "POST" {
+
+        if err := r.ParseForm(); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+		var Id = r.FormValue("Id")
+        var TaskName = r.FormValue("TaskName")
+        var Assignee = r.FormValue("Assignee")
+        var Date = r.FormValue("Date")
+
+        action := "Create"
+
+        if Id != "" {
+            action = "Update"
+            for i := range tasks {
+                if tasks[i].Id == Id {
+                    tasks[i].Id = Id
+                    tasks[i].TaskName = TaskName
+                    tasks[i].Assignee = Assignee
+                    tasks[i].Date = Date
+                }
+            }
+        } else {
+            newId := strconv.Itoa(len(tasks))
+            tasks = append(tasks, Task{Id: newId, TaskName: TaskName, Assignee:Assignee, Date:Date})
+        }
+
+        var response = Response {
+            Message : action + " Task berhasil!",
+            Success : "true",
+        }
+
+
+        var filepath = path.Join("views", "info.html")
+		var tmpl = template.Must(template.ParseFiles(filepath))
+		if err := tmpl.Execute(w, response); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+        return
+    }
+
+    http.Error(w, "", http.StatusBadRequest)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+    id := strings.TrimPrefix(r.URL.Path, "/delete/")
+
+    for i := range tasks {
+        if tasks[i].Id == id {
+            tasks[i] = tasks[len(tasks)-1]
+            tasks = tasks[:len(tasks)-1]
+        }
+    }
+
+    var response = Response {
+        Message : "Data berhasil dihapus!",
+        Success : "true",
+    }
+
+    var filepath = path.Join("views", "info.html")
+	var tmpl = template.Must(template.ParseFiles(filepath))
+	if err := tmpl.Execute(w, response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
